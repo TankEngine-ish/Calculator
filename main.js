089b83bb1489ff694ad3ecbb241ad68e0b368db4
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-10-13 00:23:44 +0300

    [feat] add basic JS functions

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
-let leftNum = Number
-let rightNum = Number
-let operator = ""
+initialValue = 0
+let displayValue = '';
+let firstNum = ''
+let secondNum = ''
+let midOperator = ''
 
 
-let displayValue = 
+function wipeDisplay();
+function undoInt();
 
-function operate (operator, two numbers);
+function adding (a, b){
+    return leftNum(a) + rightNum(b);
+}
+
+function subtracting (a, b){
+    return leftNum(a) - rightNum(b);
+}
+function multiply (a, b){
+    return leftNum(a) * rightNum(b);
+}
+function divide (a, b) {
+    if (rightNum(b) == 0){
+        return "system32 deleted - well done!"
+    }else {
+        return leftNum(a) / rightNum(b);
+    }
+}
+
+
+
+function operate (operator, two numbers) {
+    // if + call adding, if - call subtracting
+calling one of the above functions.
+}
+
+
+function populateLower ()
+function populateUpper ()
+
+const upperPart = document.getElementById('previousCalc');
+const lowerPart = document.getElementById('currentCalc');
+const numberButtons = document.querySelectorAll('button');

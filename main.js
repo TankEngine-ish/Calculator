e33af50248a7e2b6d6144917480d077eccd26d49
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-10-13 23:48:04 +0300

    [fix] refactor html + js

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
-initialValue = 0
-let displayValue = '';
+const previousCalc = document.getElementById('previousCalc');
+const currentCalc = document.getElementById('currentCalc');
+const calcButtons = document.querySelectorAll('button');
+const undoButton = document.getElementById('undo');
+const allClear = document.getElementById('clear');
+const decimalPoint = document.getElementById('dot');
+const equalSign = document.getElementById('equals');
+
+
+
+// initialValue = 0
+
+// let displayValue = '';
+
 let firstNum = ''
 let secondNum = ''
-let midOperator = ''
+let operation = null
+let displayRefresh = false
 
+calcButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
 
-function wipeDisplay();
-function undoInt();
-
-function adding (a, b){
+function add (a, b){
     return leftNum(a) + rightNum(b);
 }
 
-function subtracting (a, b){
+function subtract (a, b){
     return leftNum(a) - rightNum(b);
 }
+
 function multiply (a, b){
     return leftNum(a) * rightNum(b);
 }
+
 function divide (a, b) {
     if (rightNum(b) == 0){
-        return "system32 deleted - well done!"
+        return "system32 deleted - well done!";
     }else {
         return leftNum(a) / rightNum(b);
     }
 }
 
-
-
-function operate (operator, two numbers) {
-    // if + call adding, if - call subtracting
-calling one of the above functions.
+function operate (operator, num1, num2) {
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
 }
-
-
-function populateLower ()
-function populateUpper ()
-
-const upperPart = document.getElementById('previousCalc');
-const lowerPart = document.getElementById('currentCalc');
-const numberButtons = document.querySelectorAll('button');
